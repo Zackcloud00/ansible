690f081dc8a3a9f17fcc844ac21909133d2258b1
Zackcloud00 <114132995+Zackcloud00@users.noreply.github.com>
2022-10-23 00:56:09 +0700

    final

--- pubip/check-ip.sh
+++ pubip/check-ip.sh
@@ -2,7 +2,7 @@ pub=$(dig +short myip.opendns.com @resolver1.opendns.com)
 cur=$(cat ./newip)
 if [ "$pub" != "$cur" ]; then
  echo $pub > ./newip
- curl -H "Content-Type: application/json" -d '{"username": "desktop", "content": "'$pub'"}'\
+ curl -H "Content-Type: application/json" -d '{"username": "desktop", "content": "'$(cat ./newip)'"}'\
        https://discord.com/api/webhooks/......
   
 fi
