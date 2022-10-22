9eb1cc13dcbedcd749709e196b2ec8ac0d585d24
Zackcloud00 <114132995+Zackcloud00@users.noreply.github.com>
2022-10-22 21:41:21 +0700

    Update check-ip

--- pubip/check-ip.sh
+++ pubip/check-ip.sh
@@ -2,6 +2,6 @@ pub=$(dig +short myip.opendns.com @resolver1.opendns.com)
 cur=$(cat ./newip)
 if [ "$pub" != "$cur" ]; then
  curl -H "Content-Type: application/json" -d '{"username": "desktop", "content": "'$pub'"}'\
-       https://discord.com/api/webhooks/1033327216232243260/j8k4NinlyvrVi4_wDPQ17D_F8AyqAgiGUyO_Lc4i0KDPPbPQHWa6PHDWddx9X3PeVTDk
+       https://discord.com/api/webhooks/......
  echo $pub > ./newip 
 fi
